efdc2bc228e4682f33011c60af35b9272610f0b6
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-07-22 13:14:31 +0530

    Initial commit with backend and frontend structure

---
+++ backend/Smart-Plant-Management-System/src/main/java/lk/ijse/spring/smartplantmanagementsystem/SmartPlantManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.spring.smartplantmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPlantManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartPlantManagementSystemApplication.class, args);
+    }
+
+}
---
+++ backend/Smart-Plant-Management-System/src/test/java/lk/ijse/spring/smartplantmanagementsystem/SmartPlantManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.spring.smartplantmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPlantManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
