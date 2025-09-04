df7a8e308f392cd13ea5061d14282a49c3c33ca4
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-09-04 16:44:35 +0530

    create AuthController, UserRepository, AuthServise for register user

---
+++ backend/Smart-Plant-Management-System/src/main/java/lk/ijse/spring/smartplantmanagementsystem/controller/AuthController.java
@@ -0,0 +1,28 @@
+package lk.ijse.spring.smartplantmanagementsystem.controller;
+
+import lk.ijse.spring.smartplantmanagementsystem.dto.APIResponse;
+import lk.ijse.spring.smartplantmanagementsystem.dto.RegisterDTO;
+import lk.ijse.spring.smartplantmanagementsystem.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<APIResponse> registerUser (@RequestBody RegisterDTO registerDTO) {
+        return ResponseEntity.ok(
+                new APIResponse(
+                        200,
+                        "User registered successfully",
+                        authService.register(registerDTO)
+                )
+        );
+    }
+
+}
---
+++ backend/Smart-Plant-Management-System/src/main/java/lk/ijse/spring/smartplantmanagementsystem/service/AuthService.java
@@ -0,0 +1,31 @@
+package lk.ijse.spring.smartplantmanagementsystem.service;
+
+import lk.ijse.spring.smartplantmanagementsystem.dto.RegisterDTO;
+import lk.ijse.spring.smartplantmanagementsystem.entity.Role;
+import lk.ijse.spring.smartplantmanagementsystem.entity.User;
+import lk.ijse.spring.smartplantmanagementsystem.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+
+    public String register(RegisterDTO registerDTO) {
+
+        if (userRepository.findByEmail(registerDTO.getEmail()).isPresent()) {
+            throw new RuntimeException("Email Already Exists");
+        }
+
+        User user = User.builder()
+                .email(registerDTO.getEmail())
+                .password(registerDTO.getPassword())
+                .role(Role.valueOf(registerDTO.getRole()))
+                .build();
+
+        userRepository.save(user);
+        return "User Registered Successfully";
+    }
+}
