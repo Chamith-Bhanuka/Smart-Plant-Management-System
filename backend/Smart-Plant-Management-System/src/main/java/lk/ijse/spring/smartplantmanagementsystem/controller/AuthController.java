d80c797948f825dc0d1c5906be68be0a7714431e
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-09-04 21:40:47 +0530

    create AuthDTO

--- backend/Smart-Plant-Management-System/src/main/java/lk/ijse/spring/smartplantmanagementsystem/controller/AuthController.java
+++ backend/Smart-Plant-Management-System/src/main/java/lk/ijse/spring/smartplantmanagementsystem/controller/AuthController.java
@@ -1,11 +1,16 @@
 package lk.ijse.spring.smartplantmanagementsystem.controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lk.ijse.spring.smartplantmanagementsystem.dto.APIResponse;
+import lk.ijse.spring.smartplantmanagementsystem.dto.AuthDTO;
 import lk.ijse.spring.smartplantmanagementsystem.dto.RegisterDTO;
 import lk.ijse.spring.smartplantmanagementsystem.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @CrossOrigin(origins = "*")
 @RestController
---
+++ backend/Smart-Plant-Management-System/src/main/java/lk/ijse/spring/smartplantmanagementsystem/dto/AuthDTO.java
@@ -0,0 +1,9 @@
+package lk.ijse.spring.smartplantmanagementsystem.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthDTO {
+    public String email;
+    public String password;
+}
