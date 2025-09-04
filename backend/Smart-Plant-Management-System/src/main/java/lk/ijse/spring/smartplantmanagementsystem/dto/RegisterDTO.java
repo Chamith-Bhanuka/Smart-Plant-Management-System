beeac74b8433da8e7e5923ee8feda1060ee27142
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-09-04 16:00:15 +0530

    create registerDTO

---
+++ backend/Smart-Plant-Management-System/src/main/java/lk/ijse/spring/smartplantmanagementsystem/dto/RegisterDTO.java
@@ -0,0 +1,10 @@
+package lk.ijse.spring.smartplantmanagementsystem.dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterDTO {
+    private String email;
+    private String role;
+    private String password;
+}
