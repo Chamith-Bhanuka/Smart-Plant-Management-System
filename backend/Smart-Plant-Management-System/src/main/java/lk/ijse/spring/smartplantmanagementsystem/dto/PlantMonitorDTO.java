f7d62b4fa8904fe83e51a4c5611dc0f5135c1c28
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-09-06 01:37:56 +0530

    create PlantMonitorDTO

---
+++ backend/Smart-Plant-Management-System/src/main/java/lk/ijse/spring/smartplantmanagementsystem/dto/PlantMonitorDTO.java
@@ -0,0 +1,13 @@
+package lk.ijse.spring.smartplantmanagementsystem.dto;
+
+public class PlantMonitorDTO {
+    private Long plantId;
+    private String scientificName;
+    private String commonName;
+    private String imagePath;
+    private Double Score;
+
+    private Double latitude;
+    private Double longitude;
+    
+}
