93e762d33b5bf458a4560a35c23dd7dafd16f04a
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-09-05 18:41:19 +0530

    create OptimalConditionsRepository

---
+++ backend/Smart-Plant-Management-System/src/main/java/lk/ijse/spring/smartplantmanagementsystem/repository/OptimalConditionsRepository.java
@@ -0,0 +1,7 @@
+package lk.ijse.spring.smartplantmanagementsystem.repository;
+
+import lk.ijse.spring.smartplantmanagementsystem.entity.OptimalConditions;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OptimalConditionsRepository extends JpaRepository<OptimalConditions, Long> {
+}
