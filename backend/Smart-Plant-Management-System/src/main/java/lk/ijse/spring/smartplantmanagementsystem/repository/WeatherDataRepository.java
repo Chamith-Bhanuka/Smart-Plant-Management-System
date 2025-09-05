1334defe1174b9ade5629380a2a8809903effb04
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-09-06 01:34:12 +0530

    update weather data repository

--- backend/Smart-Plant-Management-System/src/main/java/lk/ijse/spring/smartplantmanagementsystem/repository/WeatherDataRepository.java
+++ backend/Smart-Plant-Management-System/src/main/java/lk/ijse/spring/smartplantmanagementsystem/repository/WeatherDataRepository.java
@@ -3,5 +3,8 @@ package lk.ijse.spring.smartplantmanagementsystem.repository;
 import lk.ijse.spring.smartplantmanagementsystem.entity.WeatherData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface WeatherDataRepository extends JpaRepository<WeatherData, Long> {
+    Optional<WeatherData> findTopByLocationIdOrderByTimestampDesc(Long locationId);
 }
