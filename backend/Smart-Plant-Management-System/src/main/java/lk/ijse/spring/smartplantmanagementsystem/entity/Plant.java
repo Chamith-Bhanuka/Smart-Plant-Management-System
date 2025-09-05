5112acfb58c6ce69e3ca8608ce0cc60211e5a2d2
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-09-05 13:49:55 +0530

    create plant entity

---
+++ backend/Smart-Plant-Management-System/src/main/java/lk/ijse/spring/smartplantmanagementsystem/entity/Plant.java
@@ -0,0 +1,24 @@
+package lk.ijse.spring.smartplantmanagementsystem.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Plant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String scientificName;
+    private String commonName;
+    private Double score;
+    private String imagePath;
+}
