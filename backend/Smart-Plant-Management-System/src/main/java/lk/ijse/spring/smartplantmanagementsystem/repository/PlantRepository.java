2f958813e74433d428c9fee08603da657585564f
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-09-06 01:32:16 +0530

    update plant repository

--- backend/Smart-Plant-Management-System/src/main/java/lk/ijse/spring/smartplantmanagementsystem/repository/PlantRepository.java
+++ backend/Smart-Plant-Management-System/src/main/java/lk/ijse/spring/smartplantmanagementsystem/repository/PlantRepository.java
@@ -1,7 +1,11 @@
 package lk.ijse.spring.smartplantmanagementsystem.repository;
 
 import lk.ijse.spring.smartplantmanagementsystem.entity.Plant;
+import lk.ijse.spring.smartplantmanagementsystem.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PlantRepository extends JpaRepository<Plant, Long> {
+    List<Plant> findByUser(User user);
 }
