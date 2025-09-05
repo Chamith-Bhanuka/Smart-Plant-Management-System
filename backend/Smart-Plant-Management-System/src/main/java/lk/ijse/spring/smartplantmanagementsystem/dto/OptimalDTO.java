9f7d2d59541b8ac6212f3bbac3116b4108180200
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-09-06 01:41:12 +0530

    create OptimalDTO

---
+++ backend/Smart-Plant-Management-System/src/main/java/lk/ijse/spring/smartplantmanagementsystem/dto/OptimalDTO.java
@@ -0,0 +1,13 @@
+package lk.ijse.spring.smartplantmanagementsystem.dto;
+
+import lombok.Data;
+
+@Data
+public class OptimalDTO {
+    private String plantName;
+    private Double idealTemperature;
+    private Double idealHumidity;
+    private Double idealRainfall;
+    private String soilType;
+    private String sunlightExposure;
+}
