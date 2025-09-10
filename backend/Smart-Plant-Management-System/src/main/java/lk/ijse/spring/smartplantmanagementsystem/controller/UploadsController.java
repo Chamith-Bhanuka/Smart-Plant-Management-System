aea25c7b93cc50b4e76efc100e5d83ac36e5ded0
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-09-10 22:37:17 +0530

    create UploadsController

---
+++ backend/Smart-Plant-Management-System/src/main/java/lk/ijse/spring/smartplantmanagementsystem/controller/UploadsController.java
@@ -0,0 +1,31 @@
+package lk.ijse.spring.smartplantmanagementsystem.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+@RequiredArgsConstructor
+public class UploadsController {
+    @Value("${file.upload-dir}") private String uploadDir;
+
+    @GetMapping("/uploads/{filename:.+}")
+    public ResponseEntity<Resource> getFile(@PathVariable String filename) {
+        Path file = Paths.get(uploadDir).resolve(filename).normalize();
+        if (!Files.exists(file)) return ResponseEntity.notFound().build();
+        String contentType = "image/jpeg";
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .body(new FileSystemResource(file.toFile()));
+    }
+}
